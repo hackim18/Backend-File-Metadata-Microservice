6701ffc41e8ae5e258955e383a01b8887109f928
Hackim18 <khakim668@gmail.com>
2024-07-13 23:36:58 +0700

    Finish

---
+++ Backend-File-Metadata-Microservice/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+
+app.use(cors());
+app.use("/public", express.static(__dirname + "/public/"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + this.address().port);
+});
