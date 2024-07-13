8b0b23908eeee2588a3f715fbf999b90619b64e5
Hackim18 <khakim668@gmail.com>
2024-07-13 23:47:11 +0700

    fix: fix style.css

--- Backend-File-Metadata-Microservice/index.js
+++ Backend-File-Metadata-Microservice/index.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/public", express.static(__dirname + "/public/"));
-// app.use(express.static(__dirname + "/public/"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
